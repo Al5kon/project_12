7b94dd252a335578e7694b1907d3b3f582fe09de
Alexey Konkov <alkonkov@rambler.ru>
2019-10-13 20:50:09 +0500

    Create routers

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 /* eslint-disable quotes */
 /* eslint-disable func-names */
@@ -10,59 +11,16 @@ const express = require('express');
 
 const path = require('path');
 
-const fs = require('fs');
+const router = require('./routes/users.js');
+
+const routerCards = require('./routes/cards.js');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/users', (req, res) => {
-  const userPath = path.join(__dirname, './data/users.json');
-  fs.readFile(userPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.send(data);
-    res.end();
-  });
-});
-
-app.get('/cards', (req, res) => {
-  const CardPath = path.join(__dirname, './data/cards.json');
-  fs.readFile(CardPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.send(data);
-    res.end();
-  });
-});
-
-
-app.get('/users/:_id', (req, res) => {
-  const userPath = path.join(__dirname, './data/users.json');
-
-  fs.readFile(userPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const users = JSON.parse(data);
-    let success = false;
-    for (let i = 0; i < users.length; i++) {
-      if (req.params._id === users[i]._id) {
-        res.json(users[i]);
-        success = true;
-      }
-    }
-    if (!success) {
-      res.status(404).json({ "message": "Нет пользователя с таким id" });
-    }
-  });
-});
-
+app.use('/users', router);
+app.use('/cards', routerCards);
 app.use((req, res) => {
   res.status(404).json({ "message": "Запрашиваемый ресурс не найден" });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,44 @@
-// const router = require('express').Router();
+/* eslint-disable no-useless-return */
+/* eslint-disable quote-props */
+/* eslint-disable quotes */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-plusplus */
+const router = require('express').Router();
+
+const path = require('path');
+
+const fs = require('fs');
+
+const userPath = path.join(__dirname, '../data/users.json');
+function errMessage(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+}
+
+router.use('/:_id', (req, res) => {
+  fs.readFile(userPath, { encoding: 'utf8' }, (err, data) => {
+    errMessage(err);
+    const users = JSON.parse(data);
+    let success = false;
+    for (let i = 0; i < users.length; i++) {
+      if (req.params._id === users[i]._id) {
+        res.json(users[i]);
+        success = true;
+      }
+    }
+    if (!success) {
+      res.status(404).json({ "message": "Нет пользователя с таким id" });
+    }
+  });
+});
+
+router.use('/', (req, res) => {
+  fs.readFile(userPath, { encoding: 'utf8' }, (err, data) => {
+    errMessage(err);
+    res.send(data);
+  });
+});
+
+module.exports = router;
