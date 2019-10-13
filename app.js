d1797248b139ea92b1491b67f59abe30176ac261
Alexey Konkov <alkonkov@rambler.ru>
2019-10-13 13:36:59 +0500

    Add handlers

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+/* eslint-disable func-names */
 /* eslint-disable no-unused-vars */
+/* eslint-disable quote-props */
+/* eslint-disable consistent-return */
+/* eslint-disable no-plusplus */
+
 const express = require('express');
 
 const path = require('path');
@@ -20,6 +26,45 @@ app.get('/users', (req, res) => {
     res.send(data);
     res.end();
   });
+});
+
+app.get('/cards', (req, res) => {
+  const CardPath = path.join(__dirname, './data/cards.json');
+  fs.readFile(CardPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(data);
+    res.end();
+  });
+});
+
+
+app.get('/users/:_id', (req, res) => {
+  const userPath = path.join(__dirname, './data/users.json');
+
+  fs.readFile(userPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const users = JSON.parse(data);
+    let success = false;
+    for (let i = 0; i < users.length; i++) {
+      if (req.params._id === users[i]._id) {
+        res.json(users[i]);
+        success = true;
+      }
+    }
+    if (!success) {
+      res.status(404).json({ "message": "Нет пользователя с таким id" });
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ "message": "Запрашиваемый ресурс не найден" });
 });
 
 app.listen(PORT, () => {
